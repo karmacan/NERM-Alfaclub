efc281efe3cd71a3558d59eac9434605ae4d45bd
Admin <karmacan@mail.ru>
2019-11-26 23:21:22 +0300

    _dev_ routes separated

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-require('./app/main.js');
+////////////////////////////////////////
+// SERVER
+
+const express = require('express');
+const server = express();
+const port = 5000;
+
+server.listen(port, () => { 
+  console.log(`_dev_ App started on port ${port}!`);
+});
+
+////////////////////////////////////////
+// DB
+
+const config = require('config');
+const mongoPath = config.get('mongoPath');
+
+const mongoose = require('mongoose');
+const opts = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(mongoPath, opts)
+  .then(() => { console.log('_dev_ DB connection success!') })
+  .catch((err) => { console.log(`_dev_ ${err}`) });
+
+////////////////////////////////////////
+// ROUTES
+
+const routerBase = '/api'; // router base adding to every router path
+server.use(routerBase, require('./routes/router'));
