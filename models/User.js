84b44b17ed66a548efaaeda2bba2bb52464d440d
Admin <karmacan@mail.ru>
2019-11-27 15:24:30 +0300

    _dev_ user model crated

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  pass: {type: String, required: true},
+  avatar: {type: String},
+  regDate: {type: Date, default: Date.now()}
+});
+
+const modelName = 'user';
+
+module.exports = User = mongoose.model(modelName, UserSchema);
